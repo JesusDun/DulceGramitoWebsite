1e7d4246f621a96fa252b083b7ec6abc8643d891
JesusDun <alejandrovielmagarcia@gmail.com>
2026-05-12 13:20:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def contacto():
     return render_template('contacto.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=5000)
